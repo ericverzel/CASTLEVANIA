26610bbe846cb93f9d2cbf2683e6dd7dc7ece650
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-09 16:47:50 +0100

    Shell of a controller method & view file

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,34 @@
 class PropertiesController < ApplicationController
+
     def index
     end
+
     def show
     end
+
     def new
+      @property = Property.new
     end
+
     def create
+      @property = Property.new(property_params)
+      if @property.save
+        redirect_to property_path(@property), notice: "Un Château à loué a été créé !"
+      else
+        render :new
+      end
     end
+
     def edit
     end
     def update
     end
     def destroy
+    end
+
+    private
+
+    def property_params
+      params.require(:property).permit(:name, :location, :price, :photo)
     end
 end
