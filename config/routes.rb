4acbc6af7a1bb38c073ba423936f4f57c1e5d4fd
cd ~/code/$ericverzel/dotfiles && zsh git_setup.sh <ericziouane@gmail.com>
2021-03-09 14:34:55 +0100

    addign cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :properties do
-    resources :reservations, only [:create, :show]
+    resources :reservations, only: [:create, :show]
   end
 end
