7e67a40e5187348895dbb78b954066ab9d15d9e7
cd ~/code/$ericverzel/dotfiles && zsh git_setup.sh <ericziouane@gmail.com>
2021-03-10 09:23:54 +0100

    fixing bug on hompage with link

--- db/schema.rb
+++ db/schema.rb
@@ -74,9 +74,8 @@ ActiveRecord::Schema.define(version: 2021_03_09_173057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "properties", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "properties", "users"
   add_foreign_key "reservations", "properties"
   add_foreign_key "reservations", "users"
 end
